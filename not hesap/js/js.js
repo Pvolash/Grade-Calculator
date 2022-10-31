2235a7697101526ff11b3c1b7a085a3ba8494a66
Pvolash <31490307+Pvolash@users.noreply.github.com>
2022-10-31 21:05:25 +0300

    v.2 with 7 classes

--- not hesap/js/js.js
+++ not hesap/js/js.js
@@ -5,11 +5,17 @@ var grade1 = document.getElementById("grade1").value;
 var grade2 = document.getElementById("grade2").value;
 var grade3 = document.getElementById("grade3").value;
 var grade4 = document.getElementById("grade4").value;
+var grade5 = document.getElementById("grade5").value;
+var grade6 = document.getElementById("grade6").value;
+var grade7 = document.getElementById("grade7").value;
 
 var credit1 = Number(document.getElementById("credit1").value);
 var credit2 = Number(document.getElementById("credit2").value);
 var credit3 =Number(document.getElementById("credit3").value);
 var credit4 = Number(document.getElementById("credit4").value);
+var credit5 = Number(document.getElementById("credit5").value);
+var credit6 = Number(document.getElementById("credit6").value);
+var credit7 = Number(document.getElementById("credit7").value);
 
 
 
@@ -130,18 +136,109 @@ switch (grade4) {
         grade4=0
         break;
 };
+switch (grade5) {
+    case "AA":
+        grade5=4
+        break;
+    case "BA":
+        grade5=3.5
+        break;
+    case "BB":
+        grade5=3
+        break;
+    case "CB":
+        grade5=2.5
+        break;
+    case "CC":
+        grade5=2
+        break;
+    case "DC":
+        grade5=1.5
+        break;
+    case "DD":
+        grade5=1
+        break;
+    case "FD":
+        grade5=0.5
+        break;
+    case "FF":
+        grade5=0
+        break;
+};
+switch (grade6) {
+    case "AA":
+        grade6=4
+        break;
+    case "BA":
+        grade6=3.5
+        break;
+    case "BB":
+        grade6=3
+        break;
+    case "CB":
+        grade6=2.5
+        break;
+    case "CC":
+        grade6=2
+        break;
+    case "DC":
+        grade6=1.5
+        break;
+    case "DD":
+        grade6=1
+        break;
+    case "FD":
+        grade6=0.5
+        break;
+    case "FF":
+        grade6=0
+        break;
+};
+switch (grade7) {
+    case "AA":
+        grade7=4
+        break;
+    case "BA":
+        grade7=3.5
+        break;
+    case "BB":
+        grade7=3
+        break;
+    case "CB":
+        grade7=2.5
+        break;
+    case "CC":
+        grade7=2
+        break;
+    case "DC":
+        grade7=1.5
+        break;
+    case "DD":
+        grade7=1
+        break;
+    case "FD":
+        grade7=0.5
+        break;
+    case "FF":
+        grade7=0
+        break;
+};
 
     function topCredit () {
        let firstClass = Number(grade1*credit1) ;
        let secondClass = Number(grade2*credit2);
        let thirdClass= Number(grade3*credit3);
        let fourthClass = Number(grade4*credit4);
-       let top= firstClass + secondClass + thirdClass + fourthClass;
-       let toplamKredi= credit1 + credit2 + credit3 + credit4;
+       let fifthClass = Number(grade5*credit5);
+       let sixthClass = Number(grade6*credit6);
+       let seventhClass = Number(grade7*credit7);
+       let top= firstClass + secondClass + thirdClass + fourthClass + fifthClass + sixthClass + seventhClass;;
+       let toplamKredi= credit1 + credit2 + credit3 + credit4 + credit5 + credit6 + credit7;
        let anan= top/toplamKredi;
        
        
        console.log(anan);
+       document.getElementById("output").innerHTML = anan
         return anan;
     };
     
